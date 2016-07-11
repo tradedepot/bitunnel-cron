6533f00b140385e7ce27fbb06138c2f5b481b427
Kingsley Ochu <kochu@c2gconsulting.com>
2016-07-11 11:17:16 +0100

    Add send to mail

---
+++ utils/mail-template.js
@@ -0,0 +1,61 @@
+'use strict';
+
+exports.mailTemplate = errorArray => `
+ <style>
+  .bt-error {
+    border: 1px solid #ddd;
+    border-collapse: collapse;
+    padding: 5px;
+    border-spacing: 0;
+    width: 100%;
+  }
+  
+  .bt-error th {
+    border: 1px solid #ddd;
+    padding: 5px;
+    background: #F0F0F0;
+  }
+  
+  .bt-error td {
+    border: 1px solid #ddd;
+    padding: 5px;
+  }
+  </style>
+<p>
+	Hello,
+</p>
+<p>
+	Kindly review the following errors thrown within the period under review
+</p>
+<div style="overflow-x:auto;">
+		<table class="bt-error">
+	    <thead>
+	      <tr>
+	        <th>Time</th>
+	        <th>Exception Type</th>
+	        <th>Ref</th>
+	        <th>Message</th>
+	        <th>Start Point</th>
+	        <th>Failure Point</th>
+	        <th>Origin User</th>
+	      </tr>
+	    </thead>
+	    <tbody>
+	    ${errorArray.map(err => `
+	      <tr>
+	        <td>${err.time}</td>
+	        <td>${err.exceptionType}</td>
+	        <td>${err.objectReference || 'Nil'}</td>
+	        <td>${err.message}</td>
+	        <td>${err.startPoint}</td>
+	        <td>${err.failurePoint}</td>
+	        <td>${err.originUser || 'Nil'}</td>
+	      </tr>
+	      `)}
+	    </tbody>
+	</table>
+</div>
+<p>
+The Bitunnel Team.
+</p>
+`;
---
+++ utils/mail.js
@@ -0,0 +1,36 @@
+'use strict';
+const _ = require('underscore'),
+  mailer = require('nodemailer'),
+  tpl = require('./mail-template');
+
+process.env.MAIL_URL = process.env.MAIL_URL || 'smtps://' + process.env.ODIO_EMAIL + ':' + process.env.ODIO_EMAIL_PASSWORD + '@smtp.gmail.com';
+
+// create reusable transporter object using the default SMTP transport 
+const transporter = mailer.createTransport(process.env.MAIL_URL);
+
+// setup e-mail data with unicode symbols 
+let mailOptions = {
+  from: '"Bitunnel Team 👥" <no-reply@bitunnel.io>', // sender address 
+  subject: 'Bitunnel Error Notification ✔', // Subject line 
+  text: 'Please, reopen on a browser that supports html. Thanks', // plaintext body
+};
+
+exports.sendMail = (users, message) => {
+  return new Promise((res, rej) => {
+    let userMails = _.map(users, user => {
+      return user.email;
+    })
+    userMails = userMails.toString();
+
+    mailOptions = _.extend(mailOptions, { to: userMails });
+    mailOptions = _.extend(mailOptions, { html: tpl.mailTemplate(message) });
+
+    // send mail with defined transport object 
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        rej(error);
+      }
+      res(info);
+    });
+  });
+}

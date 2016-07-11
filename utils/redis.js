ce9b0944e8dba14762b331859c72e4001de636f5
Kingsley Ochu <kochu@c2gconsulting.com>
2016-07-11 11:17:48 +0100

    Fetch admin detail from redis

---
+++ utils/redis.js
@@ -0,0 +1,37 @@
+'use strict';
+const _ = require('underscore'),
+  redis = require('redis');
+
+const redisClient = redis.createClient(
+  /*
+    process.env.REDIS_PORT, process.env.REDIS_MASTER
+  */
+)
+
+exports.getRecipientUserIds = () => {
+  return new Promise((res, rej) => {
+    redisClient.smembers('bitunnel-admin-users', (err, r) => {
+      if (err) rej(err);
+      res(r);
+    });
+  });
+}
+
+exports.getUserNameEmails = (userIds) => {
+  let result = [];
+  _.each(userIds, (userId) => {
+    let data = new Promise((res, rej) => {
+      redisClient.hmget(userId, 'name', 'email', (err, data) => {
+        if (err) rej(err);
+        data = {
+        	name: data[0],
+        	email: data[1]
+        }
+        res(data);
+      })
+    });
+    result.push(data);
+  })
+
+  return Promise.all(result);
+}
